3cd1d01c1155b0aab460abd3da730018543c20e5
dinoburgoss <58801549+dinoburgoss@users.noreply.github.com>
2025-04-19 19:58:51 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,10 @@
 // src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+// Configuración de Firebase usando variables de entorno para CRA (prefijo REACT_APP_)
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
@@ -12,7 +14,10 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
+// Inicialización de Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exportaciones para usar en otros archivos
 export const db = getFirestore(app);
 export const auth = getAuth(app);
 export default app;
